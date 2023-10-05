aefb7b7bd0b1b65f740ef8c56633bfe88873cd22
IanKamau <theurikamau7@gmail.com>
2023-10-05 20:58:10 +0300

    completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,42 @@
 #!/usr/bin/env python3
 
+# Question 1
 def greet_programmer():
-    pass
+   print("Hello, programmer!")
 
+
+# Question 2
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
-def greet_with_default(name="programmer"):
-    pass
+greet("Naureen")
 
+
+# Question 3
+def greet_with_default(name = "programmer"):
+    print(f"Hello, {name}!")
+
+greet_with_default("Sunny")
+
+
+# Question 4
 def add(num1, num2):
-    pass
+    sum = num1 + num2
+    return sum
 
-def halve(number):
-    pass
+result = add(5, 10)
+print(result)
+
+
+# Question 5
+def halve(input_value):
+    if type(input_value) in (int, float):
+        return input_value / 2
+    else:
+        return None
+
+result = halve(4)
+print(result)
+
+result = halve("two")
+print(result)
